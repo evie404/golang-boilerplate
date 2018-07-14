c3e3a87006d54edc724b3bbe8df008879980cc51
Ricky Pai <rickyp999+github@gmail.com>
2018-07-14 08:25:16 -0700

    working client

--- client/main.go
+++ client/main.go
@@ -1,22 +1,61 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package main
 
 import (
 	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/rickypai/golang-boilerplate/common"
 	pb "github.com/rickypai/golang-boilerplate/protobufs/helloworld"
-	//  1.............................. 2....... 3.......
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 )
 
+const (
+	host        = "127.0.0.1"
+	defaultName = "world"
+)
+
 func main() {
-	host := "127.0.0.1"
 	address := fmt.Sprintf("%s:%v", host, common.Port)
+
+	// Set up a connection to the server.
 	conn, err := grpc.Dial(address, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
-	pb.NewGreeterClient(conn)
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
 }
